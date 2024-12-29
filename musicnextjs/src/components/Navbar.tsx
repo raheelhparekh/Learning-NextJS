cb32736644c791d7338d81bf9f4dfba64612e213
raheelhparekh <rhparekh2003@gmail.com>
2024-12-29 20:25:23 +0530

    add Navbar, HeroSection, FeaturedSection components and utility functions; update package dependencies and Tailwind configuration

---
+++ musicnextjs/src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+"use client"; // react hooks u need to use , but nextjs does not support it, therefore we need to 'use client' to use it. Now its a client component
+
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+        <MenuItem setActive={setActive} active={active} item="Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">Course 1</HoveredLink>
+            <HoveredLink href="/courses">Course 2</HoveredLink>
+            <HoveredLink href="/courses">Course 3</HoveredLink>
+          </div>
+        </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
+}
+
+export default Navbar;
